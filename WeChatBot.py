c86bee4b6fd28fd2ea466ca2c4d5c68f8d7028a3
Jiez <jieztee@gmail.com>
2019-06-11 21:23:47 +0800

    prototype

--- WeChatBot.py
+++ WeChatBot.py
@@ -1,4 +1,12 @@
 import itchat
 from chatterbot import ChatBot
 
-bot = ChatBot('LittleJie')
+@itchat.msg_register(itchat.content.TEXT)
+def text_reply(msg):
+    bot = ChatBot('LittleJie')
+    print('{0} : {1}'.format(msg.user['NickName'], msg.text))
+    response = bot.get_response(msg.text)
+    return response
+
+itchat.auto_login()
+itchat.run()
--- train.py
+++ train.py
@@ -1,1 +1,9 @@
-from chatbot import chatbot
+from WeChatBot import bot
+from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+corpus_train = ChatterBotCorpusTrainer(bot)
+
+corpus_train.train("chatterbot.corpus.chinese")
+corpus_train.train("chatterbot.corpus.chinese.greetings")
+corpus_train.train("chatterbot.corpus.chinese.conversations")
